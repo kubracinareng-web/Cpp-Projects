3a2e21a2833ad8e094d4a39597e4dbb6e7d62e95
kubracinareng-web <kubracinar.eng@gmail.com>
2026-09-02 12:39:17 +0300

    Create for loop.cpp

---
+++ for loop.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+using namespace std;;
+
+int main() {
+	int sayi;
+
+	cout << "==== CARPIM TABLOSU =====" << endl;
+	cout << "\nBir sayi girin : ";
+	cin >> sayi;
+
+	cout << "\n" << sayi << " sayisinin carpim tablosu : " << endl;
+
+	for (int i = 1; i <= 100 ; i++) {
+		cout << sayi << " x " << i << " = " << (sayi * i) << endl;
+	}
+	return 0;
+}
+
+
+
+
